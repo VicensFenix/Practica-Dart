a94abd1e0687097fdfede6310ac50da8f3c52dbe
Vicente <vicentebravo22131@gmail.com>
2025-01-23 14:06:05 -0600

    Proyectos de practica

---
+++ bin/ciclo_for.dart
@@ -0,0 +1,6 @@
+void main() {
+  //Ciclo for para imprimir números del 1 al 100
+  for (int i = 1; i <= 10; i++) {
+    print("Número: $i");
+  }
+}
---
+++ bin/ciclo_while.dart
@@ -0,0 +1,9 @@
+void main() {
+  int i = 1; // Inicializamos la variable
+
+  // Ciclo while para imprimir números del 1 al 10
+  while (i <= 10) {
+    print('Número: $i');
+    i++; // Incrementamos la variable en cada iteración
+  }
+}
